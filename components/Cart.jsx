343ef88b028fdae785ca2ef8337b4e25b6e5c84d
Momcilo Mikovic <miiikovic19@gmail.com>
2023-04-25 22:10:57 +0200

    successfully implemented stripe payment

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -6,10 +6,32 @@ import toast from 'react-hot-toast';
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove} = useStateContext();
+  
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
+  
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -19,7 +41,7 @@ const Cart = () => {
           onClick={() => setShowCart(false)}
         >
           <AiOutlineLeft />
-          <span>Your Cart</span> 
+          <span className="heading">Your Cart</span> 
           <span className='cart-num-items'>({totalQuantities} items)</span>
 
         </button>
@@ -41,7 +63,7 @@ const Cart = () => {
         )}
 
         <div className='product-container'>
-          {cartItems.length >= 1 && cartItems.map((item, index) => (
+          {cartItems.length >= 1 && cartItems.map((item) => (
             <div className="product" key={item._id}>
               <img src={urlFor(item?.image[0])} className='cart-product-image' />
               <div className='item-desc'>
@@ -77,7 +99,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div> 
             <div className='btn-container'>
-              <button type='button' className='btn' onClick="">Pay with Stripe</button>
+              <button type='button' className='btn' onClick={handleCheckout}>Pay with Stripe</button>
             </div>  
           </div>
         )}
